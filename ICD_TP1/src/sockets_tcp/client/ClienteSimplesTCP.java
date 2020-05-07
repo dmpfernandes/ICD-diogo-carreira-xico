f66846c07a6c20ceeb47ef0d7fbb7a67d63a96f0
Miguel Carreira <miguel.carreira7@gmail.com>
2020-05-07 23:03:01 +0100

    initial structure

---
+++ ICD_TP1/src/sockets_tcp/client/ClienteSimplesTCP.java
@@ -0,0 +1,87 @@
+package sockets_tcp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class ClienteSimplesTCP {
+
+    public final static String DEFAULT_HOSTNAME = "localhost";
+    
+    public final static int DEFAULT_PORT = 5025; 
+    
+    
+    public static void main(String[] args) {
+
+        String host = DEFAULT_HOSTNAME;  // M�quina onde reside a aplica��o servidora
+        int    port = DEFAULT_PORT;      // Porto da aplica��o servidora
+
+        
+        if (args.length > 0) {
+            host = args[0];
+        }
+        
+        if (args.length > 1) {
+            try {
+                port = Integer.parseInt(args[1]);
+                if (port < 1 || port > 65535) port = DEFAULT_PORT;
+            }
+            catch (NumberFormatException e) {
+                System.err.println("Erro no porto indicado");
+            }
+        }
+        
+        
+        System.out.println("-> " + host + ":" + port );
+        
+        
+        Socket socket     = null;
+        BufferedReader is = null;
+        PrintWriter    os = null;
+        
+        try {
+            socket = new Socket(host, port);
+
+            // Mostrar os parametros da liga��o
+            System.out.println("Liga��o: " + socket);
+
+            // Stream para escrita no socket
+            os = new PrintWriter(socket.getOutputStream(), true); 
+
+            // Stream para leitura do socket
+            is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            // Escreve no socket
+            os.println("Ol� mundo!!!");
+
+            // Mostrar o que se recebe do socket
+            System.out.println("Recebi -> " + is.readLine()); 
+
+            
+            
+        } 
+        catch (IOException e) {
+            System.err.println("Erro na liga��o " + e.getMessage());   
+        }
+        finally {
+            // No fim de tudo, fechar os streams e o socket
+            try {
+                if (os != null) os.close();
+                if (is != null) is.close();
+                if (socket != null ) socket.close();
+            }
+            catch (IOException e) { 
+                // if an I/O error occurs when closing this socket
+            }
+        } // end finally
+
+
+    } // end main
+
+} // end ClienteSimplesTCP
+
+
+
