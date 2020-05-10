8c6b85c42bbb16a819f968748c24ec4e959fd2fb
Miguel Carreira <miguel.carreira7@gmail.com>
2020-05-10 15:36:33 +0100

    files for project structure

--- ICD_TP1/src/sockets_tcp/client/ClienteSimplesTCP.java
+++ ICD_TP1/src/sockets_tcp/client/ClienteSimplesTCP.java
@@ -1,4 +1,4 @@
-package sockets_tcp;
+package sockets_tcp.client;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- ICD_TP1/src/sockets_tcp/server/ServidorTCPConcorrente.java
+++ ICD_TP1/src/sockets_tcp/server/ServidorTCPConcorrente.java
@@ -1,4 +1,4 @@
-package sockets_tcp;
+package sockets_tcp.server;
 
 
 import java.io.BufferedReader;
