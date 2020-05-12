a4ae992f51b9d896263f954095fb5f9d36e1bd09
Diogo <dmpf130@gmail.com>
2020-05-13 00:12:24 +0100

    start implementing client menu

--- src/messages/RegistrationRequest.java
+++ src/messages/RegistrationRequest.java
@@ -2,21 +2,24 @@ package messages;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement
 public class RegistrationRequest {
 	
-		private String name;
-		private String username;
-		private String email;
-		private String birthDate;
-		private String password;
-		
-		public RegistrationRequest(String name, String username, String email, String password) {
-			this.name = name;
-			this.username = username;
-			this.email = email;
-			this.birthDate = birthDate;
-			this.password = password;
-		}
+	private String name;
+	private String username;
+	private String email;
+	private String birthDate;
+	private String password;
+
+	public RegistrationRequest(String name, String username, String email, String birthDate, String password) {
+		this.name = name;
+		this.username = username;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.password = password;
+	}
 }
--- src/sockets_tcp/client/ClienteSimplesTCP.java
+++ src/sockets_tcp/client/ClienteSimplesTCP.java
@@ -3,10 +3,18 @@ package sockets_tcp.client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
-import jdk.nashorn.internal.parser.JSONParser;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import messages.RegistrationRequest;
+
 
 
 public class ClienteSimplesTCP {
@@ -14,7 +22,9 @@ public class ClienteSimplesTCP {
     public final static String DEFAULT_HOSTNAME = "localhost";
     
     public final static int DEFAULT_PORT = 5025; 
-    
+    private static Socket socket = null;
+    private static BufferedReader is = null;
+    private static PrintWriter os = null;
     
     public static void main(String[] args) {
 
@@ -39,16 +49,13 @@ public class ClienteSimplesTCP {
         
         System.out.println("-> " + host + ":" + port );
         
-        
-        Socket socket     = null;
-        BufferedReader is = null;
-        PrintWriter    os = null;
+
         
         try {
             socket = new Socket(host, port);
 
             // Mostrar os parametros da liga��o
-            System.out.println("Liga��o: " + socket);
+            System.out.println("Ligação: " + socket);
 
             // Stream para escrita no socket
             os = new PrintWriter(socket.getOutputStream(), true); 
@@ -56,11 +63,7 @@ public class ClienteSimplesTCP {
             // Stream para leitura do socket
             is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            // Escreve no socket
-            os.println("Ol� mundo!!!");
-
-            // Mostrar o que se recebe do socket
-            System.out.println("Recebi -> " + is.readLine()); 
+            printMenu();
 
             
             
@@ -82,6 +85,94 @@ public class ClienteSimplesTCP {
 
 
     } // end main
+    
+    
+    private static void printMenu() {
+    	Scanner sc = new Scanner(System.in);
+    	initialMenu();
+    	int state = sc.nextInt();
+    	switch (state) {
+		case 1:
+			registrationMenu(sc);
+			break;
+		case 2:
+			
+			break;
+		default:
+			break;
+		}
+    	
+    }
+    
+    private static void initialMenu() {
+    	System.out.println("**********************");
+    	System.out.println("*         Menu       *");
+    	System.out.println("* 1 - Registration   *");
+    	System.out.println("* 2 - Login          *");
+    	System.out.println("**********************");
+    }
+    
+    private static void registrationMenu(Scanner sc) {
+    	
+    	System.out.println("**********************");
+    	System.out.println("*    Registration    *");
+    	System.out.println("*  Name              *");
+    	String name = sc.next();
+    	System.out.println("*  Username          *");
+    	String username = sc.next();
+    	System.out.println("*  Email             *");
+    	String email = sc.next();
+    	System.out.println("*  Date of Birth     *");
+    	String dob = sc.next();
+    	System.out.println("*  Password          *");
+    	String pass = sc.next();
+    	System.out.println("**********************");
+    	
+    	JAXBContext jaxbContext;
+		try {
+			jaxbContext = JAXBContext.newInstance( RegistrationRequest.class );
+			Marshaller jaxbMarshaller   = jaxbContext.createMarshaller();
+	    	RegistrationRequest rr = new RegistrationRequest(name,username,email,dob,pass);
+	    	
+	    	StringWriter sw = new StringWriter();
+	    	jaxbMarshaller.marshal(rr, sw);
+	    	String xmlString = sw.toString();
+	    	post(xmlString);
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    }
+    
+    public static void post(String out) {
+		// Escreve no socket
+        os.println(out);
+	}
+	
+	public static void clear() {
+		os.flush();
+	}
+	
+	public static String get() {
+		String in = "";
+		try { in = (String) is.readLine(); }
+		catch (IOException e) {}
+		return in;
+	}
+	
+	public static void closeChannel() {
+		try {
+			os.close(); 
+			is.close();
+			socket.close();
+		}
+		catch (IOException e) { 
+			 e.printStackTrace(); 
+		}
+	}
+    
+    
 
 } // end ClienteSimplesTCP
 
